8797c4fbfd47012edf4ef5c44f9baba80192226c
jadatix <andrijlasenko@gmail.com>
2022-11-16 16:07:18 +0200

    fixes #16: add authorization using SpringSecurity

---
+++ src/main/java/org/jadatix/carbooking/service/JpaUserDetailsService.java
@@ -0,0 +1,39 @@
+package org.jadatix.carbooking.service;
+
+
+import org.jadatix.carbooking.dao.UserDao;
+import org.jadatix.carbooking.exception.UserNotFoundException;
+import org.jadatix.carbooking.model.SecurityUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/** Class for user auth
+ *
+ */
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+    private UserDao userDao;
+
+    @Autowired
+    public JpaUserDetailsService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    /**
+     * Here email is used instead of Username
+     * @param email an email
+     * @return user casted to SecurityUser which implements UserDetails interface
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userDao
+                .findByEmail(email)
+                .map(SecurityUser::new)
+                .orElseThrow(UserNotFoundException::new);
+    }
+}
