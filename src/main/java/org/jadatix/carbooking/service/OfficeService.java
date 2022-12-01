873e8bbe1e6a988f2a27f458568bb78d8184dd1e
jadatix <andrijlasenko@gmail.com>
2022-12-01 15:55:01 +0200

    more tests for office

--- src/main/java/org/jadatix/carbooking/service/OfficeService.java
+++ src/main/java/org/jadatix/carbooking/service/OfficeService.java
@@ -2,6 +2,7 @@ package org.jadatix.carbooking.service;
 
 import org.jadatix.carbooking.dao.Dao;
 import org.jadatix.carbooking.dao.OfficeDao;
+import org.jadatix.carbooking.exception.AccessDeniedException;
 import org.jadatix.carbooking.model.Office;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,22 @@ public class OfficeService extends AbstractService<Office> {
     @Override
     protected Dao<Office> getDao() {
         return dao;
+    }
+
+    @Override
+    public Office update(Office office){
+        if( !AuthenticateUserService.isManager() ){
+            throw new AccessDeniedException();
+        }
+        return getDao().update(office);
+    }
+
+    @Override
+    public void delete(Long id) {
+        if( !AuthenticateUserService.isManager() ){
+            throw new AccessDeniedException();
+        }
+        getDao().delete(id);
     }
 
 }
